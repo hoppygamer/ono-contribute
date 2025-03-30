fc08f18bada67697e4144d1a7e2350e4084a07a5
hoppygamer <hoppygamer44roblox@gmail.com>
2025-03-30 14:15:15 +0300

    idk what i fixed, but here ig

--- src/main.ts
+++ src/main.ts
@@ -7,46 +7,34 @@ type OnoColor = {
     fore: ForegroundColorName
 }
 
-/**
- * Options for generating a logger
- */
 export type Ono = {
     color: ColorName | OnoColor,
     name: string
 };
 
-function isOnoColor(value: any): value is OnoColor {
+function isOnoColor(value: unknown): value is OnoColor {
     return (
-	value &&
-	typeof value == "object" &&
-	value.back &&
-	value.fore &&
-	backgroundColorNames.includes(value.back) &&
-	foregroundColorNames.includes(value.fore)
-    )
+        typeof value === "object" &&
+        value !== null &&
+        "back" in value && 
+        "fore" in value &&
+        backgroundColorNames.includes((value as OnoColor).back) &&
+        foregroundColorNames.includes((value as OnoColor).fore)
+    );
 }
 
-/**
- * The returned function by an ono({}) generator
- */
 export type OnoFunction = (...message: string[]) => void
 
-/**
- * Ono generator - the main way to make a logger
- * @param ono Options for ono
- * @returns The generated logger function
- */
 export function ono(ono: Ono): OnoFunction {
     let colorFunc;
     if (isOnoColor(ono.color)) {
-	const color = ono.color as OnoColor;
-	colorFunc = (a: string) => chalk[color.back as ColorName](chalk[color.fore as ColorName](a));
+        const color = ono.color;
+        colorFunc = (a: string) => chalk[color.back as ColorName](chalk[color.fore as ColorName](a));
     } else {
-	colorFunc = (a: string) => chalk[ono.color as ColorName](a);
+        colorFunc = (a: string) => chalk[ono.color as ColorName](a);
     }
 
     return function(...message: string[]) {
-	console.log(colorFunc(`[${ono.name}]`), ...message);
+        console.log(colorFunc(`[${ono.name}]`), ...message);
     }
 }
-###
